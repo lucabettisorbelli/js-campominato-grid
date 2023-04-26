eab781c5e4d95fbddd919c0cdc5192fec882c202
luca <Luca betti sorbelli>
2023-04-26 15:35:30 +0200

    visibility: hidden

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-alert("ciao")
+
+let bottone = document.getElementById("bottone");
+
+bottone.addEventListener("click", function () {
+    
+    const griglia = document.getElementById("grid");
+    griglia.classList.toggle("hidden");
+    for (let i = 0; i < 100; i++) {
+
+    
+    const nuovoDiv = document.createElement("div");
+    nuovoDiv.classList.add("square");
+
+    nuovoDiv.addEventListener("click", function() {
+        console.log("this: ", this);
+        this.classList.toggle("clicked");
+    });
+
+    griglia.appendChild(nuovoDiv);
+
+}
+
+});
+
+
