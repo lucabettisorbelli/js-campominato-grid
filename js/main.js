968bf15a0020d768f40ead173b241c43f12c9e16
luca <Luca betti sorbelli>
2023-04-26 16:51:28 +0200

    griglia con 100 div

--- js/main.js
+++ js/main.js
@@ -5,16 +5,21 @@ griglia.classList.add("hidden");
 
 
 function creaGriglia() {
-    for (let i = 0; i < 100; i++) {
-        
+    for (let i = 1; i <= 100; i++) {
+        // nuova casella
         const nuovoDiv = document.createElement("div");
         nuovoDiv.classList.add("square");
-
+        // elemento numerico in casella
+        const nuovoSpan = document.createElement("span");
+        nuovoSpan.innerText = i;
+        nuovoDiv.appendChild(nuovoSpan);
 
         nuovoDiv.addEventListener("click", function () {
-            console.log("this: ", this);
-
-            this.classList.toggle("clicked");
+            // se l'elemento contiene "clicked"
+            if (!this.classList.contains("clicked")) {
+                console.log(this.innerText);
+            }
+            this.classList.toggle("clicked"); // toggle = (add-remove)
         });
 
         griglia.appendChild(nuovoDiv);
