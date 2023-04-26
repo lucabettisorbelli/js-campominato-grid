4ee3cfe5472ce4c5405d99f2ca2e13c110467091
luca <Luca betti sorbelli>
2023-04-26 16:19:12 +0200

    bottone "hidden"

--- js/main.js
+++ js/main.js
@@ -1,25 +1,31 @@
 
 let bottone = document.getElementById("bottone");
+const griglia = document.getElementById("grid");
+griglia.classList.add("hidden");
 
-bottone.addEventListener("click", function () {
-    
-    const griglia = document.getElementById("grid");
-    griglia.classList.toggle("hidden");
+
+function creaGriglia() {
     for (let i = 0; i < 100; i++) {
+        
+        const nuovoDiv = document.createElement("div");
+        nuovoDiv.classList.add("square");
 
-    
-    const nuovoDiv = document.createElement("div");
-    nuovoDiv.classList.add("square");
 
-    nuovoDiv.addEventListener("click", function() {
-        console.log("this: ", this);
-        this.classList.toggle("clicked");
-    });
+        nuovoDiv.addEventListener("click", function () {
+            console.log("this: ", this);
 
-    griglia.appendChild(nuovoDiv);
+            this.classList.toggle("clicked");
+        });
 
+        griglia.appendChild(nuovoDiv);
+        
+    }
 }
 
+creaGriglia ();
+
+bottone.addEventListener("click", function () {
+    griglia.classList.toggle("hidden")
 });
 
 
