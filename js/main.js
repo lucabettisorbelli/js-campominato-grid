ca230673baa97791663f9276d467515f3bfba37f
luca betti sorbelli <luca betti sorbelli>
2023-04-27 13:03:07 +0200

    fix "dimensioni per numero celle"

--- js/main.js
+++ js/main.js
@@ -3,12 +3,26 @@ let bottone = document.getElementById("bottone");
 const griglia = document.getElementById("grid");
 griglia.classList.add("hidden"); // griglia invisibile di default
 
+bottone.addEventListener("click", function () {
+    griglia.innerHTML=""; // rimuove elementi
+    const select = document.getElementById("select");
+    const numeroCelle = select.value; // numero celle = valore select
+    creaGriglia (numeroCelle);
+    griglia.classList.toggle("hidden");
+});
+
 
 function creaGriglia(numeroCelle) {
     for (let i = 1; i <= numeroCelle; i++) {
         // nuova casella
         const nuovoDiv = document.createElement("div");
         nuovoDiv.classList.add("square");
+
+        // dimensioni per numerCelle
+        let radiceCelle = Math.sqrt(numeroCelle);
+        nuovoDiv.style.width = `calc(100% / ${radiceCelle})`;
+        nuovoDiv.style.height = `calc(100% / ${radiceCelle})`;
+        
         // elemento numerico in casella
         const nuovoSpan = document.createElement("span");
         nuovoSpan.innerText = i;
@@ -30,13 +44,7 @@ function creaGriglia(numeroCelle) {
 
 
 
-bottone.addEventListener("click", function () {
-    griglia.innerHTML=""; // rimuove elementi
-    const select = document.getElementById("select");
-    const numeroCelle = select.value; // numero celle = valore select
-    creaGriglia (numeroCelle);
-    griglia.classList.toggle("hidden");
-});
+
 
 
 
